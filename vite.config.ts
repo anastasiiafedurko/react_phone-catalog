96c19deed90dc1fcf258d8436d62c3471dfa18cb
“Saface89” <anastasiiafedurko@gmail.com>
2025-10-29 13:18:55 +0100

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,57 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ManifestOptions, VitePWA } from "vite-plugin-pwa";
+
+const manifest: Partial<ManifestOptions> | false = {
+  theme_color: "#9e0081",
+  background_color: "#ffffff",
+  icons: [
+    {
+      purpose: "maskable",
+      sizes: "512x512",
+      src: "icon512_maskable.png",
+      type: "image/png",
+    },
+    {
+      purpose: "any",
+      sizes: "512x512",
+      src: "icon512_rounded.png",
+      type: "image/png",
+    },
+  ],
+  screenshots: [
+    {
+      src: "/screenshots/desktop.png",
+      type: "image/png",
+      sizes: "2268 × 1420",
+      form_factor: "wide",
+    },
+    {
+      src: "/screenshots/mobile.png",
+      type: "image/png",
+      sizes: "918 × 1246",
+      form_factor: "narrow",
+    },
+  ],
+  orientation: "any",
+  display: "standalone",
+  lang: "ru",
+  name: "Nice Gadgets",
+  short_name: "Gadgets",
+  start_url: "/",
+};
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{html,css,js,ico,png,svg}"],
+      },
+      manifest: manifest,
+    }),
+  ],
   base: mode === "production" ? "/react_phone-catalog/" : "/",
 }));
