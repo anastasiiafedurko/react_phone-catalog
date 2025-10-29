c75c85cb594fb6b2980a48241da2d17b9c532061
“Saface89” <anastasiiafedurko@gmail.com>
2025-10-29 15:47:18 +0100

    images

--- vite.config.ts
+++ vite.config.ts
@@ -21,13 +21,13 @@ const manifest: Partial<ManifestOptions> | false = {
   ],
   screenshots: [
     {
-      src: "/screenshots/desktop.png",
+      src: "screenshots/desktop.png",
       type: "image/png",
       sizes: "1701x1065",
       form_factor: "wide",
     },
     {
-      src: "/screenshots/mobile.png",
+      src: "screenshots/mobile.png",
       type: "image/png",
       sizes: "689x935",
       form_factor: "narrow",
